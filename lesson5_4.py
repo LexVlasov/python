976fd03fc2be2890dac47732be17791857a6b6bf
lexvlasov <gentlemanasv@gmail.com>
2020-05-23 13:00:11 +0300

    задание 4

---
+++ lesson5_4.py
@@ -0,0 +1,15 @@
+#задание 4
+with open('lesson5.txt', encoding='utf-8') as f:
+    lines = f.readlines()
+
+with open('Новый файл.txt', 'w', encoding='utf-8') as f:
+    for line in lines:
+        if '1' in line:
+            line = line.replace('One', 'Один')
+        elif '2' in line:
+            line = line.replace('Two', 'Два')
+        elif '3' in line:
+            line = line.replace('Three', 'Три')
+        elif '4' in line:
+            line = line.replace('Four', 'Четыре')
+        f.write(line)
