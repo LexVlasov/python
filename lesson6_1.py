273e239f20a7f3bbf247fcab224c405ad84ff062
lexvlasov <gentlemanasv@gmail.com>
2020-05-24 11:34:13 +0300

    задание 1

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,18 +1,15 @@
 # задание 1
-import time
+from time import sleep
+from itertools import cycle
 
 class TrafficLight:
+    def __init__(self):
+        self.__color = (('Red', 5), ('Yellow', 2), ('Green', 4))
+
     def running(self):
-        while True:
-            print('Красный')
-            time.sleep(7)
-            print('Желтый')
-            time.sleep(2)
-            print('Зеленый')
-            time.sleep(5)
-            print('Желтый')
-            time.sleep(2)
+        for color, sec in cycle(self.__color):
+            print(color, '(wait {} sec)'.format(sec))
+            sleep(sec)
 
-
-svetofor = TrafficLight()
-print(svetofor.running())
+traffic_light = TrafficLight()
+traffic_light.running()
