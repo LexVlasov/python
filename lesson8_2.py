05902e3d27a49f6d1e6a842f3a6f74fb996a48c4
lexvlasov <gentlemanasv@gmail.com>
2020-06-01 22:27:21 +0300

    задание 2

---
+++ lesson8_2.py
@@ -0,0 +1,15 @@
+class DellNull():
+    def __init__(self, x):
+        self.x = x
+
+    def __truediv__(self, other):
+        try:
+            return float(self.x / other.x, 2)
+        except:
+            return 'Бесконечное число'
+
+a = DellNull(1)
+b = DellNull(0)
+
+print(a/b)
+
